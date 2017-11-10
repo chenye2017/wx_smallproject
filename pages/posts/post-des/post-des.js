419c95552adceb007ca5b866e2badbae2efd31ee
chenye2017 <1967196626@qq.com>
2017-11-11 01:18:19 +0800

    文章详情页面的制作

--- pages/posts/post-des/post-des.js
+++ pages/posts/post-des/post-des.js
@@ -1,75 +1,71 @@
-var post_data = require('../../data/posts-data.js');
-
+// pages/posts/post-des/post-des.js
+var dataContent = require('../../../data/posts-data.js');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    date: "Sep 12 2016"
+  
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    var id = options.id;
+    var content = dataContent.postList[id];
     this.setData({
-      post_content : post_data.postList
-    });
+      content:content
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+  
   },
+
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+  
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+  
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+  
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
-  },
-  onInfo: function (event) {
-    wx.navigateTo({
-      url: 'post-des/post-des?id=' + event.currentTarget.dataset.postid
-    })
-    
+  
   }
 })
