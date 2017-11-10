d29671f6c9eadb477521a4b5b7182f2685d67208
chenye2017 <1967196626@qq.com>
2017-11-10 20:28:17 +0800

    小程序新闻列表页面制作，包含一个swiper组件和一个自己写的template

--- pages/posts/posts.js
+++ pages/posts/posts.js
@@ -1,10 +1,12 @@
+var post_data = require('../../data/posts-data.js');
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    
+    date: "Sep 12 2016"
   },
 
   /**
@@ -12,59 +14,56 @@ Page({
    */
   onLoad: function (options) {
     
+    this.setData({
+      post_content : post_data.postList
+    });
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
-  },
 
+  },
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
-  },
-  onTap : function () {
-    wx.redirectTo({
-      url: '../posts/posts',
-    })
+
   }
 })
